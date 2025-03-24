6b5277638c4ed43e467f80569331727ebe36b56a
govindchavan12 <chavang07892@gmail.com>
2025-03-25 00:18:48 +0530

    Initial commit

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import React from "react";
+import StockEntryApp from "./components/StockEntryApp";
+
+function App() {
+  return (
+    <div className="p-4">
+      <StockEntryApp />
+    </div>
+  );
+}
+
+export default App;
